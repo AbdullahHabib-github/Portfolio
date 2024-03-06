18615695bb919d0d98b0d92dfeb447cc31e507ad
Abdullah Habib <abdullahhabib.86@gmail.com>
2024-03-06 13:42:57 +0000

    first commit by abdullah

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -10,7 +10,7 @@ import React from "react";
  * About background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that
  * represents what you see in that image.
  *
  * Need an image? Check out https://unsplash.com to download a image you
@@ -23,8 +23,7 @@ const imageAltText = "purple and blue abstract background";
 /**
  * Sort description that expands on your title on the Home component.
  */
-const description =
-  "I'm an AI Engineer studying at Ghulam Ishaq Khan Institute.";
+const description = "I'm an AI Engineer studying at Ghulam Ishaq Khan Institute.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
